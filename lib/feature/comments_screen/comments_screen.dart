1c587d9f522da4f032fa53516f2fc84b7dcb65ab
Your Name <abd233661@gmail.com>
2022-08-11 22:27:15 +0300

    take

--- lib/feature/comments_screen/comments_screen.dart
+++ lib/feature/comments_screen/comments_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CommentsScreen extends StatelessWidget {
-  const CommentsScreen({Key? key}) : super(key: key);
+class CommentsScreen extends StatefulWidget {
+   CommentsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CommentsScreen> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+List <String>Comments =[];
+
+void addComments(String val){
+  setState(() {
+    Comments.add(val);  
+  });
+
+  Widget BuildCommentsList(){
+    return ListView.builder(itemBuilder: (context, index) {
+      if(index <Comments.length){
+        return BuildCommentsList();
+      }
+    }));
+  }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text("Comments",style: TextStyle(fontSize: 20.0,color: Colors.black45),),
+      ),
+      body: ,
+    );
   }
 }
