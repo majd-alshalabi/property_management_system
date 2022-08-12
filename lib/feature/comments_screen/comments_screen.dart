270f092a27b3161aee8e06961e799366d0c2074a
Your Name <abd233661@gmail.com>
2022-08-12 16:23:09 +0300

    take

--- lib/feature/comments_screen/comments_screen.dart
+++ lib/feature/comments_screen/comments_screen.dart
@@ -1,38 +1,111 @@
+import 'package:comment_box/comment/comment.dart';
 import 'package:flutter/material.dart';
 
-class CommentsScreen extends StatefulWidget {
-   CommentsScreen({Key? key}) : super(key: key);
-
+class TestMe extends StatefulWidget {
   @override
-  State<CommentsScreen> createState() => _CommentsScreenState();
+  _TestMeState createState() => _TestMeState();
 }
 
-class _CommentsScreenState extends State<CommentsScreen> {
-List <String>Comments =[];
+class _TestMeState extends State<TestMe> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController commentController = TextEditingController();
+  List filedata = [
+    {
+      'name': 'Adeleye Ayodeji',
+      'pic': 'https://picsum.photos/300/30',
+      'message': 'I love to code'
+    },
+    {
+      'name': 'Biggi Man',
+      'pic': 'https://picsum.photos/300/30',
+      'message': 'Very cool'
+    },
+    {
+      'name': 'Biggi Man',
+      'pic': 'https://picsum.photos/300/30',
+      'message': 'Very cool'
+    },
+    {
+      'name': 'Biggi Man',
+      'pic': 'https://picsum.photos/300/30',
+      'message': 'Very cool'
+    },
+  ];
 
-void addComments(String val){
-  setState(() {
-    Comments.add(val);  
-  });
-
-  Widget BuildCommentsList(){
-    return ListView.builder(itemBuilder: (context, index) {
-      if(index <Comments.length){
-        return BuildCommentsList();
-      }
-    }));
-  }
+  Widget commentChild(data) {
+    return ListView(
+      children: [
+        for (var i = 0; i < data.length; i++)
+          Padding(
+            padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
+            child: ListTile(
+              leading: GestureDetector(
+                onTap: () async {
+                  // Display the image in large form.
+                  print("Comment Clicked");
+                },
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: new BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: new BorderRadius.all(Radius.circular(50))),
+                  child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(data[i]['pic'] + "$i")),
+                ),
+              ),
+              title: Text(
+                data[i]['name'],
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(data[i]['message']),
+            ),
+          )
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        title: Text("Comments",style: TextStyle(fontSize: 20.0,color: Colors.black45),),
+        title: Text("Comment Page"),
+        backgroundColor: Colors.green,
       ),
-      body: ,
+      body: Container(
+        child: CommentBox(
+          userImage:
+              "https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400",
+          child: commentChild(filedata),
+          labelText: 'Write a comment...',
+          withBorder: false,
+          errorText: 'Comment cannot be blank',
+          sendButtonMethod: () {
+            if (formKey.currentState!.validate()) {
+              print(commentController.text);
+              setState(() {
+                var value = {
+                  'name': 'New User',
+                  'pic':
+                      'https://lh3.googleusercontent.com/a-/AOh14GjRHcaendrf6gU5fPIVd8GIl1OgblrMMvGUoCBj4g=s400',
+                  'message': commentController.text
+                };
+                filedata.insert(0, value);
+              });
+              commentController.clear();
+              FocusScope.of(context).unfocus();
+            } else {
+              print("Not validated");
+            }
+          },
+          formKey: formKey,
+          commentController: commentController,
+          backgroundColor: Colors.black54,
+          textColor: Colors.white,
+          sendWidget: Icon(Icons.send_sharp, size: 30, color: Colors.green),
+        ),
+      ),
     );
   }
 }
