8faa618ffb87641b296d9bc54cfb588cd61ba1fe
Your Name <abd233661@gmail.com>
2022-08-12 19:24:39 +0300

    take

--- lib/feature/details_screen/details_screen.dart
+++ lib/feature/details_screen/details_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:property_management_system/core/componets/components.dart';
 import 'package:property_management_system/feature/comments_screen/comments_screen.dart';
 import 'package:property_management_system/feature/details_screen/bloc/details_cubit.dart';
+import 'package:property_management_system/feature/edit_screen/edit_screen.dart';
 import 'package:property_management_system/injection_container.dart';
 
 class DetailsScreen extends StatefulWidget {
@@ -16,12 +17,6 @@ class DetailsScreen extends StatefulWidget {
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
-  final List<String> images = [
-    "assets/images/123.jpg",
-    "assets/images/123.jpg",
-    "assets/images/123.jpg",
-  ];
-
   @override
   void initState() {
     super.initState();
@@ -42,100 +37,196 @@ class _DetailsScreenState extends State<DetailsScreen> {
         builder: (context, state) {
           if (bloc.propertyDetailsModel != null)
             return Scaffold(
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditScreen(),
+                        ));
+                  },
+                  child: Icon(Icons.edit),
+                ),
                 body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                      height: 300.0,
-                      child: Swiper(
-                        itemBuilder: (BuildContext context, int index) {
-                          return Image(
-                            image: AdvancedNetworkImage(
-                              bloc.propertyDetailsModel!.photo![index],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          height: 300.0,
+                          child: Swiper(
+                            itemBuilder: (BuildContext context, int index) {
+                              return Image(
+                                image: AdvancedNetworkImage(
+                                  bloc.propertyDetailsModel!.photo![index],
+                                ),
+                                fit: BoxFit.fill,
+                              );
+                            },
+                            autoplay: true,
+                            itemCount: bloc.propertyDetailsModel!.photo!.length,
+                            pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
                             ),
-                            fit: BoxFit.fill,
-                          );
-                        },
-                        autoplay: true,
-                        itemCount: bloc.propertyDetailsModel!.photo!.length,
-                        pagination: const SwiperPagination(
-                          alignment: Alignment.bottomCenter,
+                            control: const SwiperControl(
+                                color: Colors.black87,
+                                disableColor: Colors.black),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              "State :",
+                              style: TextStyle(color: Colors.green),
+                            ),
+                            SizedBox(
+                              width: 8.0,
+                            ),
+                            Text(
+                              bloc.propertyDetailsModel!.state.toString(),
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            Spacer(),
+                            Text(bloc.propertyDetailsModel!.viewCount
+                                .toString()),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Icon(
+                              Icons.remove_red_eye,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Text("4.3"),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            Icon(
+                              Icons.star_rate,
+                              color: Colors.yellow,
+                            ),
+                          ],
                         ),
-                        control: const SwiperControl(
-                            color: Colors.black87, disableColor: Colors.black),
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Row(
-                      children: [
-                        Spacer(),
-                        Text(bloc.propertyDetailsModel!.viewCount.toString()),
-                        SizedBox(
-                          width: 5.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "Price",
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
                         ),
-                        Icon(
-                          Icons.remove_red_eye,
-                          color: Colors.green,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.00),
+                        child: Text(
+                          bloc.propertyDetailsModel!.price.toString() + "\$",
+                          style: TextStyle(color: Colors.green),
                         ),
-                        SizedBox(
-                          width: 20.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black54),
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.bedroom_child,
+                                    color: Colors.green,
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    bloc.propertyDetailsModel!.bedroomnumber
+                                        .toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black54),
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.bathroom,
+                                    color: Colors.green,
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    bloc.propertyDetailsModel!.bathroomnumber
+                                        .toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black54),
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.home,
+                                    color: Colors.green,
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    bloc.propertyDetailsModel!.propartytype
+                                        .toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                          ],
                         ),
-                        Text("4.3"),
-                        SizedBox(
-                          width: 5.0,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          "Description",
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
                         ),
-                        Icon(
-                          Icons.star_rate,
-                          color: Colors.yellow,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Text(
+                          bloc.propertyDetailsModel!.description ?? "",
+                          style: TextStyle(color: Colors.black54, fontSize: 15),
                         ),
-                      ],
-                    ),
+                      ),
+                      newbutton(
+                          function: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: ((context) => TestMe())));
+                          },
+                          height: 50.0,
+                          text: "comments",
+                          width: double.infinity,
+                          color: Colors.white,
+                          colortext: Colors.green,
+                          colorRaduis: Colors.green)
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      "Price",
-                      style: TextStyle(color: Colors.black, fontSize: 20.0),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.00),
-                    child: Text(
-                      bloc.propertyDetailsModel!.price.toString() + "\$",
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text(
-                      "Description",
-                      style: TextStyle(color: Colors.black, fontSize: 20.0),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      bloc.propertyDetailsModel!.description ?? "",
-                      style: TextStyle(color: Colors.black54, fontSize: 15),
-                    ),
-                  ),
-                  newbutton(
-                      function: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: ((context) => TestMe())));
-                      },
-                      height: 50.0,
-                      text: "comments",
-                      width: double.infinity,
-                      color: Colors.white,
-                      colortext: Colors.green,
-                      colorRaduis: Colors.green)
-                ],
-              ),
-            ));
+                ));
           return Scaffold(
             body: Center(
               child: CircularProgressIndicator(),
